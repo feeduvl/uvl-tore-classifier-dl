5f56768b7e938fe4d00096d4e552da04fa70a502
emilydickins <eileen.dickson@hotmail.de>
2022-07-27 11:57:20 +0200

    Training and Testing of model ready

--- src/flask_setup/routes.py
+++ src/flask_setup/routes.py
@@ -1,33 +1,36 @@
 import datetime
 
 from flask import Flask, request, json, jsonify
-from nltk.tag import StanfordNERTagger
-from nltk.tokenize import word_tokenize
 
 from src.flask_setup import app
 
 
-@app.route('/hitec/classify/concepts/deep-ner/run', methods=['POST'])
+@app.route('/hitec/classify/concepts/bi-lstm-classifier/run', methods=['POST'])
 def classify_tore():
 
-    app.logger.info('Deep-NER Classification run requested')
-    app.logger.debug('/hitec/classify/concepts/deep-ner/run called')
+    app.logger.info('Bi-LSTM Classification run requested')
+    app.logger.debug('/hitec/classify/concepts/bi-lstm-classifier/run called')
     timestamp = '{:%Y-%m-%d_%H%M%S-%f}'.format(datetime.datetime.now())
 
     content = json.loads(request.data.decode('utf-8'))
-    # app.logger.info(content)
+    app.logger.info(content)
 
-    dataset = content["dataset"]["documents"]
+    documents = content["dataset"]["documents"]
+    dataset_name = content["dataset"]["name"]
+    annotation_name = content["params"]["annotation_name"]
+    create = content["params"]["persist"] == 'true'
 
-    # Send request to get selected annotation
-    # Ths should be a parameter when running algorithm
+    app.logger.info(f'Create settings: {create}, {type(create)}')
 
-    # app.logger.info(dataset)
+    annotation_handler = AnnotationHandler(annotation_name, dataset_name, app.logger)
+    request_handler = RequestHandler(app.logger, annotation_handler)
+    codes = request_handler.process(documents, create)
 
-    return 'OK'
+    result = dict()
+    result.update({"codes": codes})
+    return jsonify(result)
 
-
-@app.route('/hitec/classify/concepts/deep-ner/status', methods=["GET"])
+@app.route('/hitec/classify/concepts/bi-lstm-classifier/status', methods=["GET"])
 def get_status():
     status = {
         "status": "operational",
